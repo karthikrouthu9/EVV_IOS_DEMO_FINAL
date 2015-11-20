d260bee02c224f5d4ff089bf025e4f6177ff91f6
karthikrouthu9 <routhukarthik93@gmail.com>
2015-11-20 00:41:43 -0600

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,17 +1,29 @@
 	$(document).ready(function(){
-		alert("Doc Ready");
+		
+/*	alert("Doc Ready First");
+	
+	
+	var date = new Date();
+	alert("time zones"+date.toTimeString());
+	var removev=date.toTimeString();
+	var v=removev.replace(/\s/g, '');
+	var zonetiming = v.substr(0,v.indexOf('G'));
+	zone=v.match(/\((.*?)\)/)[1];
+	alert("zone"+zone);*/
+	
+	
 	
     document.addEventListener("deviceready",onDeviceReady,false);     
 
 });
 	
 	function exit_app() {
-		alert("Exit App");
+
 		  navigator.app.exitApp();
 		}
 		 
 	
-/*	 function resolution_handling() 
+	 function resolution_handling() 
 	 {
 	     //first way to implement
 	     browser_width = $(window).width();
@@ -19,10 +31,9 @@
 	     //alert('browser_width'+browser_width);
 	     //alert('browser_height'+browser_height);
 	 }
-*/
+
 		function onDeviceReady() {
-		alert("DR");
-			window.plugins.uniqueDeviceID.get(success, fail);
+			
  		//var element = document.getElementById('deviceProperties');
 		//var device_uuid = device.uuid;
        
@@ -37,38 +48,88 @@
     }
     else
 {
-    	alert("Else Loop");
     	
-}
-} 		
+    	alert("else loop");
+		window.plugins.uniqueDeviceID.get(success, fail);
 		
 		
+		
+		
+		
+		$.ajaxSetup({
+        xhrFields: {
+            withCredentials: true
+        }
+    });
+
+		$.ajax({
+          url:"http://183.82.96.212:8080/services/session/token",
+          type:"get",
+          dataType:"text",
+          crossDomain: true,
+          timeout: 20000,
+          error:function (jqXHR, textStatus, errorThrown) {
+        	  if(textStatus==="timeout") {
+        		  bootbox.dialog({
+        			  closeButton: false,
+        			  message: "Problem connecting with server. Please try after sometime.",
+        			  title: "Alert",
+        			  buttons: {
+        			    success: {
+        			      label: "OK",
+        			      className: "btn-danger",
+        			      callback: function() {
+        			    	  
+        			    	  exit_app();
+        			      }
+        			    
+        			    }
+        			    
+        			    
+        			  }
+        			});
+		        } 
+          },
+          success: function (token) {   
+
+   	var device_uuid = device.uuid;
+	var d = document.getElementById("device_uuid");
+	var token =token;
+	var header = "X-CSRF-TOKEN";
+    $(document).ajaxSend(function(e, xhr, options) {
+        xhr.setRequestHeader(header, token);
+    });
 	
+
 		
-    		is_device_registered();
-   alert("Before Custom Function");
+    		//is_device_registered(device_uuid);
+   
  function is_device_registered()
 {
-		alert("Inside Custom Function");
+
 	var device_uuid = globalVariable;
 	alert("For Device Value");
 	alert(device_uuid);
+	
+
+
+
 	             $.ajax({
               url: 'http://183.82.96.212:8080/m_service/m_resources/is_device_registered',
               type: "post",
-      		  data: 'device_uuid='+device_uuid,
+      		  data: 'device_uuid='+device.uuid,
               dataType: "json",
               timeout: 20000,
               crossDomain: true,
               
               
 			  error: function (jqXHR, textStatus, errorThrown) {
-			  	alert("Error Function");
-			  	alert(jqXHR.statusText);
+			  
+			  alert(jqXHR.textStatus);
 				  if(textStatus==="timeout") {
 					  bootbox.dialog({
 						  closeButton: false,
-	        			  message: "Problem connecting with server. Please try after sometime..",
+	        			  message: "Problem connecting with server. Please try after sometime.",
 	        			  title: "Alert",
 	        			  buttons: {
 	        			    success: {
@@ -87,9 +148,8 @@
 				  }
 			        
              bootbox.dialog({
-   
             	 closeButton: false,
-  message: "Problem connecting with server. Please try after sometime....",
+  message: "Problem connecting with server. Please try after sometime.",
   title: "Alert",
   buttons: {
     success: {
@@ -106,36 +166,41 @@
   }
 });
               },
-	success: function (data) {
-			alert("Success Ajax");				
-	//data.logindata[0].is_security_question_answered;
-	if(data.is_security_question_answered==1 && data.logindata[0].count>=1)
+			                success: function (data) {
+							
+			                	//data.logindata[0].is_security_question_answered;
+							if(data.is_security_question_answered==1 && data.logindata[0].count>=1)
       {
-   	alert("Already Registered");
+   
       window.location='./log-in.html';
       return false;
       }
       else if(data.is_security_question_answered==0 && data.logindata[0].count>=1)
       {
-      	alert("Enter Security question and answer");
       window.location='./portal_security_questions.html?user_id='+data.user_id;
       return false;
       }
       else
       {
-      	alert("Registration");
       window.location='./registration.html';
       return false;
       }
 										  }
             });
 
-  //return false;
+  return false;
 
 }
-  		
- 
-    var globalVariable;			
+}
+			});
+}
+}
+    			
+				
+				
+				
+				
+				var globalVariable;			
      function success(uuid)		
 {
 	alert("Before uuid");
@@ -149,8 +214,4 @@ function fail(uuid)
 {		
    		
     alert("failure function reg");		
-};			
-    			
-    			
-    			
-    			
+};		
